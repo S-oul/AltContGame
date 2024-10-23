0c1070e87f8aceb69b0fb98fa4f8736fdd881942
Nicolas Kohler <nicolas.kohler@free.fr>
2024-10-23 19:58:25 +0200

    [ADD] hands anim on players 1 and 2

--- Gangstamancer/Assets/Scripts/HandsSpriteOnPlayers.cs
+++ Gangstamancer/Assets/Scripts/HandsSpriteOnPlayers.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
 
 public class HandsSpriteOnPlayers : MonoBehaviour
@@ -9,6 +10,7 @@ public class HandsSpriteOnPlayers : MonoBehaviour
     private Animator handPlayer1;
     private Animator handPlayer2;
 
+    [SerializeField] private List<Fingers> _allFingers = new List<Fingers>();
 
     private void Start()
     {
@@ -17,50 +19,47 @@ public class HandsSpriteOnPlayers : MonoBehaviour
     }
     public void DiplayPlayer1Hands(bool display)
     {
+        if (!display) return;
         _handsOnPlayer1.SetActive(display);
     }
 
     public void DiplayPlayer2Hands(bool display)
     {
+        if (!display) return;
         _handsOnPlayer2.SetActive(display);
     }
 
     public void PutGoodHandsOnPlayer1(HandsSign handSign)
     {
-        SendTriggerToAnimator(handPlayer1, handSign.handSignLeft.name);
+        Debug.Log("PutGoodHandsOnPlayer1");
+        SendTriggerToAnimator(handPlayer1, handSign.handSignLeft);
         _handsOnPlayer1.transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = handSign.handSignLeft.SpriteLeft;
         _handsOnPlayer1.transform.GetChild(1).GetComponent<SpriteRenderer>().sprite = handSign.handSignRight.SpriteRight;
     }
 
     public void PutGoodHandsOnPlayer2(HandsSign handSign)
     {
-        SendTriggerToAnimator(handPlayer2, handSign.handSignLeft.name);
+        Debug.Log("PutGoodHandsOnPlayer2");
+        SendTriggerToAnimator(handPlayer2, handSign.handSignLeft);
         _handsOnPlayer2.transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = handSign.handSignLeft.SpriteLeft;
         _handsOnPlayer2.transform.GetChild(1).GetComponent<SpriteRenderer>().sprite = handSign.handSignRight.SpriteRight;
     }
 
-    private void SendTriggerToAnimator(Animator animator, string name)
+    private void SendTriggerToAnimator(Animator animator, Fingers finger)
     {
-        switch (name)
-        {
-            case "Call me":
-                animator.SetTrigger("Call");
-                break;
-            case "cornes du diable":
-                animator.SetTrigger("Cornes");
-                break;
-            case "doigt d'honneur":
-                animator.SetTrigger("Fuck");
-                break;
-            case "finger gun":
-                animator.SetTrigger("Gun");
-                break;
-            case "index":
-                animator.SetTrigger("Index");
-                break;
-            case "Poing":
-                animator.SetTrigger("Poing");
-                break;
-        }
+            Debug.Log(finger);
+        if (_allFingers[0] == finger)
+            animator.SetTrigger("Call");
+        else if (_allFingers[1] == finger)
+            animator.SetTrigger("Cornes");
+        else if (_allFingers[2] == finger)
+            animator.SetTrigger("Fuck");
+        else if (_allFingers[3] == finger)
+            animator.SetTrigger("Gun");
+        else if (_allFingers[4] == finger)
+            animator.SetTrigger("Index");
+        else if (_allFingers[5] == finger)
+            animator.SetTrigger("Poing");
+        
     }
 }
