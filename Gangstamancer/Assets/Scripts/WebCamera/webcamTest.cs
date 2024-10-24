414d5601ca999056f905ba2bad28ecdfbe045ac8
Sacha <S.Soulslasher@gmail.com>
2024-10-24 12:06:07 +0200

    bruh

--- Gangstamancer/Assets/Scripts/WebCamera/webcamTest.cs
+++ Gangstamancer/Assets/Scripts/WebCamera/webcamTest.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Windows.WebCam;
 
 public class webcamTest : MonoBehaviour
@@ -8,6 +9,7 @@ public class webcamTest : MonoBehaviour
 
     WebCamDevice webCam;
     public Renderer webCamRenderer;
+    public List<Texture> list = new List<Texture>();
 
     // Start is called before the first frame update
     void Start()
@@ -17,11 +19,15 @@ public class webcamTest : MonoBehaviour
         webCamRenderer = GetComponent<Renderer>();
         webCamRenderer.material.mainTexture = tt;
         tt.Play();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            list.Add(webCamRenderer.material.mainTexture);
+        }
     }
 }
